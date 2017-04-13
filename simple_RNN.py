f6449028a61007cf09e518ee35a14a7fbc59745d
govindnh4cl <govindsinghrathore@outlook.com>
2017-04-13 13:27:38 -0400

    Backup

--- simple_RNN.py
+++ simple_RNN.py
@@ -1,10 +1,23 @@
+seed = 1337  # for reproducibility
 import numpy as np
+np.random.seed(seed)        # Seed Numpy
+import random               # Seed random
+random.seed(seed)
+import tensorflow as tf
+tf.set_random_seed(seed)    # Seed Tensor Flow
+
 from scipy.signal import triang
 import matplotlib.pyplot as plt
 
 from keras.layers import Input, Dense, SimpleRNN
-from keras.models import Sequential
+from keras.models import Sequential, load_model, save_model
 from keras.optimizers import RMSprop
+
+def plot_signals(signals):
+    plt.plot(signals[:, 0])
+    plt.plot(signals[:, 1])
+    plt.grid()
+    plt.show()
 
 def get_model(batch_shape, num_hidden_units, num_output):
     model = Sequential()
@@ -13,35 +26,114 @@ def get_model(batch_shape, num_hidden_units, num_output):
     return model
 
 def compile_model(model):
-    rms = RMSprop()
+    rms = RMSprop(0.001, decay=0.0)
     model.compile(optimizer=rms, loss='mse')
 
-def train_model(m, X, y, batch_size):
-    epochs = 1
-    compile_model(m)
+def dump_train_history(history, val_set_present, log_file_name):
+    f = open(log_file_name, "w")
 
-    m.fit(X, y, nb_epoch=epochs, batch_size=batch_size, shuffle=False)
+    accuracy_present = 'acc' in history.keys()
 
+    if accuracy_present:
+        train_acc = history['acc']
+
+    train_loss = history['loss']
+
+    if val_set_present:
+        if accuracy_present:
+            val_acc = history['val_acc']
+        val_loss = history['val_loss']
+    else:
+        val_loss = [-1] * len(train_loss)
+        val_acc = [-1] * len(train_loss)
+
+    f.write('Epoch  Train_loss  Train_acc  Val_loss  Val_acc  \n')
+
+    for i in range(len(train_loss)):
+        if accuracy_present:
+            f.write('{0:d} {1:f} {2:.2f}% {3:f} {4:.2f}%\n'.format(
+                i, train_loss[i], 100 * train_acc[i], val_loss[i], 100 * val_acc[i]))
+        else:
+            f.write('{0:d} {1:f} {2:f}\n'.format(i, train_loss[i], val_loss[i]))
+
+    print('Dumped history to file: {0:s}'.format(log_file_name))
+
+def run_part_a(m, x, y, batch_size):
+    train_epochs = 100
+    test_trajectories = 10
+    train = 1 # Whether to train or load a trained model
+    model_file = 'model.h5'
+
+    # Train
+    if train:
+        compile_model(m)
+        if 0:
+            for i in range(train_epochs):
+                hist = m.fit(x, y, nb_epoch=1, batch_size=batch_size, shuffle=False, verbose=2)
+                m.reset_states()
+        else:
+            hist = m.fit(x, y, nb_epoch=train_epochs, batch_size=batch_size, shuffle=False, verbose=2)
+
+        save_model(m, model_file)
+        dump_train_history(hist.history, False, 'history_a.log')
+
+    # Test
+    if not train:
+        m = load_model(model_file)
+
+    y_pred = np.zeros([test_trajectories, len(y), 2])
+    mse = np.zeros(test_trajectories)
+
+    y_prev = np.array([0., 0.])
+    # Predict() does not use the states generated during training
+    for t in range(test_trajectories):
+        for i in range(len(x)):
+            y_pred[t, i] = m.predict_on_batch(y_prev.reshape(batch_size, 1, -1))
+            y_prev = y_pred[t, i]
+        mse[t] = np.sum(np.linalg.norm((y_pred[t] - y), axis=1) ** 2)/len(x)
+
+    print('Part (a) MSE: ', mse)
+
+    if 1:
+        plt.figure()
+        original = y
+        predicted = y_pred[9, :, :]
+        plt.plot(original[:, 0], original[:, 1]) # Original input
+        plt.plot(predicted[:, 0], predicted[:, 1])
+        plt.savefig('plot_0.jpg')
+        print(np.concatenate([original, predicted], axis=1))
+        plt.show()
+
+    return y_pred
 
 if __name__ == '__main__':
     N = 64
-    batch_size = 4
+    batch_size = 1
     time_steps = 1
     num_feature = 2 # 2 coordinates
     num_output = num_feature
+    trajectories = 1 # epochs
 
-    x = np.zeros((65, num_feature))
-    x[:, 0] = np.concatenate([[0], triang(N-1), [0]]) #Triangle wave
-    x[:, 1] = np.sin(np.linspace(0, 4 * np.pi, 65)) # Sine wave
+    signals = np.zeros((65, num_feature))
+    signals[:, 0] = np.concatenate([[0], triang(31), [0], -triang(31), [0]]) #Triangle wave
+    signals[:, 1] = np.sin(np.linspace(0, 4 * np.pi, 65)) # Sine wave
+    signals = signals[:-1, :] # Remove last sample to create a time-period
 
-    x_train = x[:-1, :]
-    y_train = x[1:, :]
-    x_train = x_train.reshape(x_train.shape[0], time_steps, num_feature)
-    y_train = y_train.reshape(x_train.shape[0], num_feature)
+    if 0:
+        plot_signals(signals)
+        exit(0)
+
+    x = signals
+    y = np.concatenate([signals[1:], signals[0].reshape(1, -1)])
+    x = x.reshape(x.shape[0], time_steps, num_feature)
+    y = y.reshape(x.shape[0], num_feature)
     batch_shape = (batch_size, time_steps, num_feature)
 
     m = get_model(batch_shape, 15, num_output)
-    m = train_model(m, x_train, y_train, batch_size)
+
+    if 1: # part(a)
+        run_part_a(m, x, y, batch_size)
+
 
 
 
